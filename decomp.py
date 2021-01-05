96d8093bed4350f3a088bad96b5ba9ee851222de
zegevlier <iamascratcher@gmail.com>
2021-01-05 15:37:55 +0100

    Decomp can now edit region files

--- decomp.py
+++ decomp.py
@@ -1,5 +1,5 @@
 import zlib
-from quarry.types.nbt import TagRoot, RegionFile
+from quarry.types.nbt import TagRoot, RegionFile, TagString
 from utils import *
 import struct
 
@@ -8,16 +8,18 @@ outfile = "close/region/r.0.0.mca"
 
 
 with open(outfile, "wb") as f:
-    data = struct.pack("b", 0)
-    all_data = b""
-    for _ in range(sector_size*2):
-        all_data+=data
-    f.write(data)
+    # data = struct.pack("b", 0)
+    # all_data = b""
+    # for _ in range(sector_size*2):
+        # all_data+=data
+    # print(all_data)
+    f.seek(sector_size*2-1)
+    f.write(b"\0")
 
 with open("r.0.0.mca", "rb") as f:
     data = f.read()
-
-chunks = [[{} for i in range(1024)] for i in range(1024)]
+ 
+chunks = [[{} for _ in range(1024)] for _ in range(1024)]
 
 for x, s in enumerate(chunks):
     for z, chunk in enumerate(s):
@@ -43,6 +45,8 @@ for x, s in enumerate(chunks):
                 raise TypeError
             parsed_nbt = TagRoot.from_bytes(chunk_data)
             del parsed_nbt.body.value["Level"].value["Biomes"]
+            parsed_nbt.body.value["Level"].value["Sections"].value[1].value["Palette"].value[0].value["Name"].value = u"minecraft:barrier"
+
             print(parsed_nbt.to_obj())
             region_file = RegionFile(outfile)
             region_file.save_chunk(parsed_nbt)
